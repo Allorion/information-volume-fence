c0a68c5cb20a05e02b10913253088e46d49f3f5a
Allori <wlad70657@gmail.com>
2022-03-17 16:45:21 +0300

    Обновлен дизайн: 1) Главная страница водопользования

--- src/pages/fact-wusage/FactWusage.jsx
+++ src/pages/fact-wusage/FactWusage.jsx
@@ -12,8 +12,8 @@ export default function FactWusage () {
     return(
         <React.Fragment>
             <Grid container sx={{textOverflow: 'ellipsis'}}>
-                <Grid item xs={0} md={2} xl={2}/>
-                <Grid item xs={12} md={8} xl={8}>
+                <Grid item xs={0} sm={0} md={1} lg={1} xl={1}/>
+                <Grid item xs={12} sm={12} md={10} lg={10} xl={10}>
                     <Paper elevation={3}>
                         <HeadBox>Фактическое водопользование</HeadBox>
                         <Box p={4}>
@@ -21,6 +21,7 @@ export default function FactWusage () {
                         </Box>
                     </Paper>
                 </Grid>
+                <Grid item xs={0} sm={0} md={1} lg={1} xl={1}/>
             </Grid>
         </React.Fragment>
     );
